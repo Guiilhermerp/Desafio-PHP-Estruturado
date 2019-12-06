89500e37ba330b52bcbc132baa3e7787f1bd9b18
Guilherme <guiigrodrigues@gmail.com>
2019-12-06 01:49:21 -0300

    Criação footer

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
+        <div class="container offset-5">
+          <p style="color:white">Feito por: Guilherme Rodrigues</p>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          </div>
+        </div>
+      </nav>
+</body>
+</html>
